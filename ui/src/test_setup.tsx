9a9a12dc3bc2ac0bbe468986ac1d0bb93d01efdb
Jocelyn Stericker <jocelyn@nettek.ca>
2022-05-24 10:34:28 -0400

    Fix locale stringg mocks

--- ui/src/test_setup.tsx
+++ ui/src/test_setup.tsx
@@ -1,20 +1,20 @@
 const toLocaleString = Date.prototype.toLocaleString;
 const toLocaleDateString = Date.prototype.toLocaleDateString;
 const toLocaleTimeString = Date.prototype.toLocaleTimeString;
-Date.prototype.toLocaleString = function (locale = "en-US", options?) {
-  return toLocaleString.call(this, "en-US", {
+Date.prototype.toLocaleString = function (locale?, options?) {
+  return toLocaleString.call(this, locale || "en-US", {
     timeZone: "UTC",
     ...(options ?? {}),
   });
 };
-Date.prototype.toLocaleTimeString = function (locale = "en-US", options?) {
-  return toLocaleTimeString.call(this, "en-US", {
+Date.prototype.toLocaleTimeString = function (locale?, options?) {
+  return toLocaleTimeString.call(this, locale || "en-US", {
     timeZone: "UTC",
     ...(options ?? {}),
   });
 };
-Date.prototype.toLocaleDateString = function (locale = "en-US", options?) {
-  return toLocaleDateString.call(this, "en-US", {
+Date.prototype.toLocaleDateString = function (locale?, options?) {
+  return toLocaleDateString.call(this, locale || "en-US", {
     timeZone: "UTC",
     ...(options ?? {}),
   });
